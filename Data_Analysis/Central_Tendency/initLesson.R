9979bb66d89d28a8c6f711f5e9262c3d867100ae
sbahrami <55531912+sbahrami@users.noreply.github.com>
2023-01-17 20:02:28 -0500

    Update initLesson.R

--- Data_Analysis/Central_Tendency/initLesson.R
+++ Data_Analysis/Central_Tendency/initLesson.R
@@ -1,2 +1,2 @@
-  assign("cars", openintro::cars, envir=globalenv())
+  assign("cars93", openintro::cars, envir=globalenv())
   assign("mpg.midsize", cars[cars$type=="midsize","mpgCity"], envir=globalenv())
